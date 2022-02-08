2ba2aacb4a92d394f55b4bcd973788702c38a92a
watchakorn-18k <porton2559@gmail.com>
2022-02-08 18:42:11 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+from flask import Flask, render_template,request
+import os
+import shutil
+# load image
+
+
+
+def removeImg(ImgOrinal):
+    
+    img = cv2.imread(ImgOrinal)
+    # convert to graky
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # threshold input image as mask
+    mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
+
+    # negate mask
+    mask = 255 - mask
+
+    # apply morphology to remove isolated extraneous noise
+    # use borderconstant of black since foreground touches the edges
+    kernel = np.ones((3,3), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    # anti-alias the mask -- blur then stretch
+    # blur alpha channel
+    mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
+
+    # linear stretch so that 127.5 goes to 0, but 255 stays 255
+    mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
+
+    # put mask into alpha channel
+    result = img.copy()
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
+    result[:, :, 3] = mask
+
+    # save resulting masked image
+    cv2.imwrite('static/test1.png', result)
+    shutil.rmtree('upload')
+    os.makedirs('upload')
+    # # display result, though it won't show transparency
+    # cv2.imshow("INPUT", img)
+    # cv2.imshow("GRAY", gray)
+    # cv2.imshow("MASK", mask)
+    # cv2.imshow("RESULT", result)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+UPLOAD_FOLDER = './upload'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    try:
+        if request.method == 'POST':
+            if 'file1' not in request.files:
+                return 'there is no file1 in form!'
+            file1 = request.files['file1']
+            path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+            file1.save(path)
+            removeImg(path)
+            return render_template('showImg.html')
+    except:
+        return render_template('index.html')
+        
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
+# removeImg(ImgOrinal='test.png')
