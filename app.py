3b82e4e308317bbfe8a666e362cdee15ce2ef4e8
watchakorn-18k <porton2559@gmail.com>
2022-02-08 19:14:22 +0700

    UPdate

--- app.py
+++ app.py
@@ -71,6 +71,5 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
 # removeImg(ImgOrinal='test.png')
